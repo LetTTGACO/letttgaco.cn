682c9ea765da15b2e34db6ca72eb58553220157a
fangpengfei <1225751694@qq.com>
2020-03-17 00:08:29 +0800

    add files

---
+++ js/index.js
@@ -0,0 +1,29 @@
+
+// var footerHeight
+// function changeFooterHeight(callback) {
+//     var footer = document.getElementsByClassName('footer')
+//     console.dir('footer', footer)
+//     var footerOffsetHeight = footer.offsetTop
+//     console.log('footerOffsetHeight', footerOffsetHeight)
+//     var clientHeight = window.innerHeight
+//     footerHeight = clientHeight - footerOffsetHeight
+//     console.log('footerHeight', footerHeight)
+//     footer.style.height = footerHeight + 'px'
+//     callback()
+// }
+
+// function changeHeaderHeight() {
+//     var header = document.getElementsByClassName('header')[0]
+//     console.dir(header)
+//     header.style.height = ((footerHeight / 2) * 1) + 'px'
+// }
+
+
+
+
+
+// changeFooterHeight(function () {
+//     changeHeaderHeight()
+// })
+
+
